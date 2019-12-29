baf09775090841972d7a0dfdfe96e74dc8bf635f
Phung Nguyen <phungn@y7mail.com>
2019-12-30 01:58:17 +1100

    Extend delay time

--- WorkerService/WorkerService/Worker.cs
+++ WorkerService/WorkerService/Worker.cs
@@ -19,7 +19,7 @@ namespace WorkerService
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(5000, stoppingToken);
             }
         }
     }
