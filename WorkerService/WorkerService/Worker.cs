24b430e743e4286763f732dc399513024227ed48
Phung Nguyen <phungn@y7mail.com>
2019-12-30 01:48:14 +1100

    Clean Code

--- WorkerService/WorkerService/Worker.cs
+++ WorkerService/WorkerService/Worker.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WorkerService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private int testCount = 0;
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
@@ -23,7 +20,6 @@ namespace WorkerService
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
-                ++testCount;
             }
         }
     }
